65a05b2c294ced580a97453806ff8437abf7cbcf
Khadija Abdulahi <khadijaabdulahi@gmail.com>
2021-10-08 15:47:33 -0500

    Classes Melon Part 1 -3

---
+++ melons.py
@@ -0,0 +1,73 @@
+"""Classes for melon orders."""
+
+class AbstractMelonOrder():
+    """Parent class for domestic and international orders"""
+
+    def __init__(self, species, qty, shipped, order_type, tax):
+        self.species = species
+        self.qty = qty
+        self.shipped = shipped
+        self.order_type = order_type
+        self.tax = tax
+
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = 5
+
+        if self.species == "Christmas melon":
+            base_price = base_price * 1.5
+
+        total = (1 + self.tax) * self.qty * base_price
+
+        if self.order_type == "international" and self.qty < 10:
+            
+            total = total + 3
+
+
+        return total
+    
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+
+    def __init__(self, species, qty, passed_inspection):
+        """Initialize melon order attributes."""
+        super().__init__(species, qty, False, "government", 0.00)  
+        self.passed_inspection = False 
+    
+    def mark_inspection(self, passed): 
+        """ Tells us if melon order has passed inspection or not """
+        self.passed_inspection = passed 
+
+
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes."""
+        super().__init__(species, qty, False, "domestic", .08)
+        
+        
+        
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    def __init__(self, species, qty, country_code):
+        super().__init__(species, qty, False, "international", 0.17)
+        """Initialize melon order attributes."""
+        
+        self.country_code = country_code # We're setting an instance attribute in this line for country_code
+
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
+
